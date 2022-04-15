f5958f4cfdb2a2e1b3d058e3143d1860cd102f3d
DiyaanahSuhaini <20ftt1445@student.pb.edu.bn>
2022-04-15 23:43:04 +0800

    dist added

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,79 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="flex items-center justify-center mt-5">
+
+	<div class="bg-emerald-900 w-1/8 p-10 h-auto">
+		<div class="flex items-center justify-center mb-10">
+			<img src="./assets/profile.jpg" alt=""
+				class="rounded-full w-32 border-2 border-gray-300">
+		</div>
+		<h1 class="text-gray-100 uppercase tracking-widest text-lg font-bold">Contact</h1>
+		<hr class="w-1/6 mb-5">
+    <h1 class="text-gray-100 text-sm">DAYANGKU NURUL DIYAANAH IZZATI BINTI PENGIRAN SUHAINI</h1>
+		<h1 class="text-gray-100 text-sm">20FTT1445@student.pb.edu.bn</h1>
+		<h1 class="text-gray-100 text-sm">+673 734 6874</h1>
+		<h1 class="text-gray-100 text-sm">Bandar Seri Begawan, Brunei Darussalam</h1>
+		<hr class="my-5">
+		<h1 class="text-gray-100 mt-2 uppercase tracking-widest text-lg font-bold">EDUCATION</h1>
+		<hr class="w-1/6 mb-5">
+		<h1 class="text-gray-100 text-sm uppercase font-semibold tracking-wider">IGCE 'O' Lvel'</h1>
+		<h1 class="text-gray-100 font-semibold italic"><span class="italic">SM PAP HM</span>, <span
+				class="text-gray-300 text-sm">Sungai Akar</span> / 2016-2019</h1>
+		<br>
+		<h1 class="text-gray-100 text-sm uppercase font-semibold tracking-wider">Level 5 Diploma in Web Development</h1>
+		<h1 class="text-gray-100 font-semibold italic"><span class="italic">Politeknik Brunei</span>, <span
+				class="text-gray-300 text-sm">OSP</span> / 2020-2023</h1>
+		<hr class="my-5">
+		<h1 class="text-gray-100 mt-2 uppercase tracking-widest text-lg font-bold">SKILLS</h1>
+		<hr class="w-1/6 mb-5">
+		<ul>
+			<li class="text-gray-300 text-sm list-disc ml-4">Communication Skills</li>
+			<li class="text-gray-300 text-sm list-disc ml-4">Creativity</li>
+			<li class="text-gray-300 text-sm list-disc ml-4">Adobe Photoshop</li>
+			<li class="text-gray-300 text-sm list-disc ml-4">Microsoft Power Point</li>
+			<li class="text-gray-300 text-sm list-disc ml-4">Microsoft Word</li>
+		</ul>
+	</div>
+
+
+
+
+
+	<div class="bg-white w-7/12 p-10 h-auto">
+		<h1 class="text-gray-800">A creative personnel with a passion for writing and designing who have a strong background in being a responsible and disciplined individual. </h1>
+		<h1 class="font-semibold uppercase tracking-wider my-6 text-gray-800">Experience</h1>
+		<h1 class="text-gray-600 font-semibold">Publicity Designer (Jan 2020 - Present)</h1>
+		<ul class="list-disc ml-5 text-gray-800">
+			<li>YuNa2riffic</li>
+			<li>Assists the designs of the artwork and the collateral.</li>
+		</ul>
+		<h1 class="text-gray-600 font-semibold mt-3">Supervisor (Jan 2020 - March 2020)</h1>
+		<ul class="list-disc ml-5 text-gray-800">
+			<li>Yuniquebeauty.</li>
+			<li>Assisted in attending both workers and clients.</li>
+		</ul>
+		<h1 class="text-gray-600 font-semibold mt-3">Product Reviewer (May 2020 - Present)</h1>
+		<ul class="list-disc ml-5 text-gray-800">
+			<li>Conducted product reviews on makeup and skincare products</li>
+		</ul>
+		<h1 class="text-gray-600 font-semibold mt-3">Representative (2016 - present)</h1>
+		<ul class="list-disc ml-5 text-gray-800">
+			<li>Awarded as the the 'Most Dedicated Student' in 2018.</li>
+			<li>Member of the Peer Facilitator' and offered to be the head of Student Council.</li>
+			<li>Bruneian representative for Strategic Education Alliances of Southeast Asian Conference 2018</li>
+      <li>School representative for Brunei International Cadet Exchange Programme as an army cadet</li>
+		</ul>
+	</div>
+</div>
 </template>
 
 <style>
 #app {
+  background-color: #ecfdf5;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
